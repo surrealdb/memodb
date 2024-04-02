1bc9a7fa211c8252796f9e7b81a78a609fd01deb
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-04-02 23:22:21 +0100

    Add initial code

---
+++ src/db.rs
@@ -0,0 +1,254 @@
+// Copyright © SurrealDB Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::err::Error;
+use crate::tx::Tx;
+use concread::bptree::BptreeMap;
+use std::fmt::Debug;
+use std::pin::Pin;
+use std::sync::Arc;
+
+pub struct Db<K, V>
+where
+	K: Ord + Clone + Debug + Sync + Send + 'static,
+	V: Eq + Clone + Sync + Send + 'static,
+{
+	pub(crate) db: Pin<Arc<BptreeMap<K, V>>>,
+}
+
+// Open a new database
+pub fn new<K, V>() -> Db<K, V>
+where
+	K: Ord + Clone + Debug + Sync + Send + 'static,
+	V: Eq + Clone + Sync + Send + 'static,
+{
+	Db {
+		db: Arc::pin(BptreeMap::new()),
+	}
+}
+
+impl<K, V> Db<K, V>
+where
+	K: Ord + Clone + Debug + Sync + Send + 'static,
+	V: Eq + Clone + Sync + Send + 'static,
+{
+	// Start a new transaction
+	pub async fn begin(&self, write: bool) -> Result<Tx<K, V>, Error> {
+		match write {
+			true => Ok(Tx::write(self.db.clone(), self.db.write())),
+			false => Ok(Tx::read(self.db.clone(), self.db.read())),
+		}
+	}
+}
+
+#[cfg(test)]
+mod tests {
+
+	use super::*;
+	use crate::kv::{Key, Val};
+
+	#[tokio::test]
+	async fn begin_tx_readable() {
+		let db: Db<Key, Val> = new();
+		let tx = db.begin(false).await;
+		assert!(tx.is_ok());
+	}
+
+	#[tokio::test]
+	async fn begin_tx_writeable() {
+		let db: Db<Key, Val> = new();
+		let tx = db.begin(true).await;
+		assert!(tx.is_ok());
+	}
+
+	#[tokio::test]
+	async fn begin_tx_readable_async() {
+		let db: Db<Key, Val> = new();
+		let tx = async { db.begin(false).await };
+		assert!(tx.await.is_ok());
+	}
+
+	#[tokio::test]
+	async fn begin_tx_writeable_async() {
+		let db: Db<Key, Val> = new();
+		let tx = async { db.begin(true).await };
+		assert!(tx.await.is_ok());
+	}
+
+	#[tokio::test]
+	async fn writeable_tx_across_async() {
+		let db: Db<&str, &str> = new();
+		let mut tx = db.begin(true).await.unwrap();
+		let res = async { tx.put("test", "something") }.await;
+		assert!(res.is_ok());
+		let res = async { tx.get("test") }.await;
+		assert!(res.is_ok());
+		let res = async { tx.commit() }.await;
+		assert!(res.is_ok());
+	}
+
+	#[tokio::test]
+	async fn readable_tx_not_writable() {
+		let db: Db<&str, &str> = new();
+		// ----------
+		let mut tx = db.begin(false).await.unwrap();
+		let res = tx.put("test", "something");
+		assert!(res.is_err());
+		let res = tx.set("test", "something");
+		assert!(res.is_err());
+		let res = tx.del("test");
+		assert!(res.is_err());
+		let res = tx.commit();
+		assert!(res.is_err());
+		let res = tx.cancel();
+		assert!(res.is_ok());
+	}
+
+	#[tokio::test]
+	async fn finished_tx_not_writeable() {
+		let db: Db<&str, &str> = new();
+		// ----------
+		let mut tx = db.begin(false).await.unwrap();
+		let res = tx.cancel();
+		assert!(res.is_ok());
+		let res = tx.put("test", "something");
+		assert!(res.is_err());
+		let res = tx.set("test", "something");
+		assert!(res.is_err());
+		let res = tx.del("test");
+		assert!(res.is_err());
+		let res = tx.commit();
+		assert!(res.is_err());
+		let res = tx.cancel();
+		assert!(res.is_err());
+	}
+
+	#[tokio::test]
+	async fn cancelled_tx_is_cancelled() {
+		let db: Db<&str, &str> = new();
+		// ----------
+		let mut tx = db.begin(true).await.unwrap();
+		tx.put("test", "something").unwrap();
+		let res = tx.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx.get("test").unwrap();
+		assert_eq!(res, Some("something"));
+		let res = tx.cancel();
+		assert!(res.is_ok());
+		// ----------
+		let mut tx = db.begin(false).await.unwrap();
+		let res = tx.exi("test").unwrap();
+		assert_eq!(res, false);
+		let res = tx.get("test").unwrap();
+		assert_eq!(res, None);
+		let res = tx.cancel();
+		assert!(res.is_ok());
+	}
+
+	#[tokio::test]
+	async fn committed_tx_is_committed() {
+		let db: Db<&str, &str> = new();
+		// ----------
+		let mut tx = db.begin(true).await.unwrap();
+		tx.put("test", "something").unwrap();
+		let res = tx.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx.get("test").unwrap();
+		assert_eq!(res, Some("something"));
+		let res = tx.commit();
+		assert!(res.is_ok());
+		// ----------
+		let mut tx = db.begin(false).await.unwrap();
+		let res = tx.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx.get("test").unwrap();
+		assert_eq!(res, Some("something"));
+		let res = tx.cancel();
+		assert!(res.is_ok());
+	}
+
+	#[tokio::test]
+	async fn multiple_concurrent_readers() {
+		let db: Db<&str, &str> = new();
+		// ----------
+		let mut tx = db.begin(true).await.unwrap();
+		tx.put("test", "something").unwrap();
+		let res = tx.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx.get("test").unwrap();
+		assert_eq!(res, Some("something"));
+		let res = tx.commit();
+		assert!(res.is_ok());
+		// ----------
+		let mut tx1 = db.begin(false).await.unwrap();
+		let res = tx1.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx1.exi("temp").unwrap();
+		assert_eq!(res, false);
+		// ----------
+		let mut tx2 = db.begin(false).await.unwrap();
+		let res = tx2.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx2.exi("temp").unwrap();
+		assert_eq!(res, false);
+		// ----------
+		let res = tx1.cancel();
+		assert!(res.is_ok());
+		let res = tx2.cancel();
+		assert!(res.is_ok());
+	}
+
+	#[tokio::test]
+	async fn multiple_concurrent_operators() {
+		let db: Db<&str, &str> = new();
+		// ----------
+		let mut tx = db.begin(true).await.unwrap();
+		tx.put("test", "something").unwrap();
+		let res = tx.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx.get("test").unwrap();
+		assert_eq!(res, Some("something"));
+		let res = tx.commit();
+		assert!(res.is_ok());
+		// ----------
+		let mut tx1 = db.begin(false).await.unwrap();
+		let res = tx1.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx1.exi("temp").unwrap();
+		assert_eq!(res, false);
+		// ----------
+		let mut txw = db.begin(true).await.unwrap();
+		txw.put("temp", "other").unwrap();
+		let res = txw.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = txw.exi("temp").unwrap();
+		assert_eq!(res, true);
+		let res = txw.commit();
+		assert!(res.is_ok());
+		// ----------
+		let mut tx2 = db.begin(false).await.unwrap();
+		let res = tx2.exi("test").unwrap();
+		assert_eq!(res, true);
+		let res = tx2.exi("temp").unwrap();
+		assert_eq!(res, true);
+		// ----------
+		let res = tx1.exi("temp").unwrap();
+		assert_eq!(res, false);
+		// ----------
+		let res = tx1.cancel();
+		assert!(res.is_ok());
+		let res = tx2.cancel();
+		assert!(res.is_ok());
+	}
+}
---
+++ src/tx.rs
@@ -0,0 +1,233 @@
+// Copyright © SurrealDB Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use crate::err::Error;
+use crate::inner::Inner;
+use concread::bptree::BptreeMap;
+use concread::bptree::BptreeMapReadTxn;
+use concread::bptree::BptreeMapWriteTxn;
+use std::fmt::Debug;
+use std::ops::Range;
+use std::pin::Pin;
+use std::sync::Arc;
+
+pub struct Tx<K, V>
+where
+	K: Ord + Clone + Debug + Sync + Send + 'static,
+	V: Eq + Clone + Sync + Send + 'static,
+{
+	// Is the transaction complete?
+	pub(crate) ok: bool,
+	// Is the transaction read+write?
+	pub(crate) rw: bool,
+	// The underlying datastore transaction
+	pub(crate) tx: Inner<K, V>,
+	// The parent datastore for this transaction
+	_db: Pin<Arc<BptreeMap<K, V>>>,
+}
+
+impl<K, V> Tx<K, V>
+where
+	K: Ord + Clone + Debug + Sync + Send + 'static,
+	V: Eq + Clone + Sync + Send + 'static,
+{
+	// Create a transaction
+	pub(crate) fn read(db: Pin<Arc<BptreeMap<K, V>>>, tx: BptreeMapReadTxn<'_, K, V>) -> Tx<K, V> {
+		let tx = unsafe {
+			std::mem::transmute::<BptreeMapReadTxn<'_, K, V>, BptreeMapReadTxn<'static, K, V>>(tx)
+		};
+		Tx {
+			ok: false,
+			rw: false,
+			tx: Inner::Read(tx),
+			_db: db,
+		}
+	}
+	// Create a transaction
+	pub(crate) fn write(
+		db: Pin<Arc<BptreeMap<K, V>>>,
+		tx: BptreeMapWriteTxn<'_, K, V>,
+	) -> Tx<K, V> {
+		let tx = unsafe {
+			std::mem::transmute::<BptreeMapWriteTxn<'_, K, V>, BptreeMapWriteTxn<'static, K, V>>(tx)
+		};
+		Tx {
+			ok: false,
+			rw: true,
+			tx: Inner::Write(tx),
+			_db: db,
+		}
+	}
+	// Check if closed
+	pub fn closed(&self) -> bool {
+		self.ok
+	}
+	// Cancel a transaction
+	pub fn cancel(&mut self) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Mark this transaction as done
+		self.ok = true;
+		// Unlock the database mutex
+		match self.tx {
+			Inner::None => unreachable!(),
+			Inner::Read(_) => self.tx = Inner::None,
+			Inner::Write(_) => self.tx = Inner::None,
+		}
+		// Continue
+		Ok(())
+	}
+	// Commit a transaction
+	pub fn commit(&mut self) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check to see if transaction is writable
+		if self.rw == false {
+			return Err(Error::TxNotWritable);
+		}
+		// Mark this transaction as done
+		self.ok = true;
+		// Commit the data
+		match std::mem::take(&mut self.tx) {
+			Inner::None => unreachable!(),
+			Inner::Read(_) => unreachable!(),
+			Inner::Write(v) => v.commit(),
+		}
+		// Continue
+		Ok(())
+	}
+	// Check if a key exists
+	pub fn exi(&self, key: K) -> Result<bool, Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check the key
+		let res = self.tx.exi(&key);
+		// Return result
+		Ok(res)
+	}
+	// Fetch a key from the database
+	pub fn get(&self, key: K) -> Result<Option<V>, Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Get the key
+		let res = self.tx.get(&key).cloned();
+		// Return result
+		Ok(res)
+	}
+	// Insert or update a key in the database
+	pub fn set(&mut self, key: K, val: V) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check to see if transaction is writable
+		if self.rw == false {
+			return Err(Error::TxNotWritable);
+		}
+		// Set the key
+		self.tx.set(key, val);
+		// Return result
+		Ok(())
+	}
+	// Insert a key if it doesn't exist in the database
+	pub fn put(&mut self, key: K, val: V) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check to see if transaction is writable
+		if self.rw == false {
+			return Err(Error::TxNotWritable);
+		}
+		// Set the key
+		match self.tx.exi(&key) {
+			false => self.tx.set(key, val),
+			_ => return Err(Error::KeyAlreadyExists),
+		};
+		// Return result
+		Ok(())
+	}
+	// Insert a key if it matches a value
+	pub fn putc(&mut self, key: K, val: V, chk: Option<V>) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check to see if transaction is writable
+		if self.rw == false {
+			return Err(Error::TxNotWritable);
+		}
+		// Set the key
+		match (self.tx.get(&key), &chk) {
+			(Some(v), Some(w)) if v == w => self.tx.set(key, val),
+			(None, None) => self.tx.set(key, val),
+			_ => return Err(Error::ValNotExpectedValue),
+		};
+		// Return result
+		Ok(())
+	}
+	// Delete a key
+	pub fn del(&mut self, key: K) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check to see if transaction is writable
+		if self.rw == false {
+			return Err(Error::TxNotWritable);
+		}
+		// Remove the key
+		self.tx.del(&key);
+		// Return result
+		Ok(())
+	}
+	// Delete a key if it matches a value
+	pub fn delc(&mut self, key: K, chk: Option<V>) -> Result<(), Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Check to see if transaction is writable
+		if self.rw == false {
+			return Err(Error::TxNotWritable);
+		}
+		// Remove the key
+		match (self.tx.get(&key), &chk) {
+			(Some(v), Some(w)) if v == w => self.tx.del(&key),
+			(None, None) => self.tx.del(&key),
+			_ => return Err(Error::ValNotExpectedValue),
+		};
+		// Return result
+		Ok(())
+	}
+	// Retrieve a range of keys from the databases
+	pub fn scan(&self, rng: Range<K>, limit: usize) -> Result<Vec<(K, V)>, Error> {
+		// Check to see if transaction is closed
+		if self.ok == true {
+			return Err(Error::TxClosed);
+		}
+		// Scan the keys
+		let res = self.tx.range(rng, limit);
+		// Return result
+		Ok(res)
+	}
+}
