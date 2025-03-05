c368ca686c8d5a622f4f36c433914fa525d50031
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-03-05 21:50:01 +0000

    Improve timestamp oracle performance

--- src/oracle.rs
+++ src/oracle.rs
@@ -1,23 +1,29 @@
 use std::sync::atomic::{AtomicU64, Ordering};
-use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{Instant, SystemTime, UNIX_EPOCH};
 
 /// A timestamp oracle for monotonically increasing time
 pub struct Oracle {
-	/// The latest timestamp for this oracle
+	/// The latest monotonic counter for this oracle
 	timestamp: AtomicU64,
+	/// The reference time when this Oracle was created
+	reference_time: Instant,
+	/// The corresponding Unix timestamp, in nanoseconds
+	reference_unix: u64,
 }
 
 impl Oracle {
 	/// Creates a new timestamp oracle
 	pub fn new() -> Self {
+		// Get the current unix time in nanoseconds
+		let reference_unix = Self::current_unix_ns();
+		// Get a new monotonically increasing clock
+		let reference_time = Instant::now();
+		// Return the current timestamp oracle
 		Self {
-			timestamp: AtomicU64::new(Self::current_time_ns()),
+			timestamp: AtomicU64::new(reference_unix),
+			reference_time,
+			reference_unix,
 		}
-	}
-
-	/// Gets the current system time in nanoseconds since the Unix epoch
-	fn current_time_ns() -> u64 {
-		SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or_default().as_nanos() as u64
 	}
 
 	/// Returns the current timestamp for this oracle
@@ -25,14 +31,24 @@ impl Oracle {
 		self.timestamp.load(Ordering::SeqCst)
 	}
 
+	/// Gets the current system time in nanoseconds since the Unix epoch
+	fn current_unix_ns() -> u64 {
+		SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or_default().as_nanos() as u64
+	}
+
+	/// Gets the current estimated time in nanoseconds since the Unix epoch
+	fn current_time_ns(&self) -> u64 {
+		self.reference_unix + self.reference_time.elapsed().as_nanos() as u64
+	}
+
 	/// Returns a monotonically increasing timestamp in nanoseconds
 	pub fn next_timestamp(&self) -> u64 {
 		// Get the current nanoseconds since the Unix epoch
-		let mut current_ts = Self::current_time_ns();
+		let mut current_ts = self.current_time_ns();
 		// Loop until we reach the next incremental timestamp
 		loop {
 			// Get the last timestamp for this oracle
-			let last_ts = self.timestamp.load(Ordering::Relaxed);
+			let last_ts = self.timestamp.load(Ordering::Acquire);
 			// Increase the timestamp to ensure monotonicity
 			if current_ts <= last_ts {
 				current_ts = last_ts + 1;
@@ -40,7 +56,7 @@ impl Oracle {
 			// Try to update last_ts atomically
 			if self
 				.timestamp
-				.compare_exchange(last_ts, current_ts, Ordering::SeqCst, Ordering::Relaxed)
+				.compare_exchange(last_ts, current_ts, Ordering::AcqRel, Ordering::Relaxed)
 				.is_ok()
 			{
 				return current_ts;
