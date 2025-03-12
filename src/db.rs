c34bc26866b4d525b0c89bb7e6e2cea5ce5d75bb
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2025-03-12 14:28:57 +0000

    Tweak

--- src/db.rs
+++ src/db.rs
@@ -31,7 +31,7 @@ where
 	K: Ord + Clone + Debug + Sync + Send + 'static,
 	V: Eq + Clone + Debug + Sync + Send + 'static,
 {
-	// The inner strcuture of an Oracle
+	// The inner strcuture of the Oracle
 	inner: Arc<Inner<K, V>>,
 }
 
